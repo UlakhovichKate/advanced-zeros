37a9c208a195231668a5f60c6e40a269fb8d8963
Katsiaryna <36642433+UlakhovichKate@users.noreply.github.com>
2018-03-04 23:18:03 +0300

    16ричная система+перевернуть масси и добавть из предыдущей задачи

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let Arr = new Array;
+  while (number/base !=0){
+    ost=number%base;
+    Arr.push(ost);
+    number=number/base;
+  }
+  return Arr;
 }
